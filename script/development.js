a7d375de12a4b8e00cc444c8b576028c408f4864
Perukii <57752033+Perukii@users.noreply.github.com>
2021-02-23 15:04:32 +0900

    新しいドキュメントを導入

---
+++ script/development.js
@@ -0,0 +1,13 @@
+window.onload = function(){
+
+    inner_text = document.getElementById("inner_text_development");
+    const d = "development/";
+    gentoggle(d+"go_tinywm");
+    gentoggle(d+"dbscan_test");
+    gentoggle(d+"mitewm");
+    gentoggle(d+"sanmitsuwm");
+    gentoggle(d+"cairo_picker");
+    gentoggle(d+"cairo_sparkle");
+    gentoggle(d+"perugtk");
+
+}
---
+++ script/research.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+
+    inner_text = document.getElementById("inner_text_research");
+    const d = "research/";
+    gentoggle(d+"railwaysim");
+    gentoggle(d+"curtainrail");
+    gentoggle(d+"mulpsi");
+
+}
